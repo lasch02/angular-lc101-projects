5e6633f82843ca18c3fb44d74f3805c57cd8f77f
Linda Schasch <lasch02@sbcglobal.net>
2023-03-01 15:29:03 -0600

    completed exercise chapter 30

--- lesson2/exercises/src/app/candidates/candidates.component.ts
+++ lesson2/exercises/src/app/candidates/candidates.component.ts
@@ -25,8 +25,14 @@ export class CandidatesComponent implements OnInit {
 
   // Code the addToCrew function here:
 
-
+addToCrew(candidate: object) {
+  if (!this.crew.includes(candidate)) {
+    this.crew.push(candidate);
+  }
+}
   // BONUS: Code the changeMissionName function here:
-
+changeMissionName(replacementName: string) {
+  this.missionName = replacementName
+}
 
 }
